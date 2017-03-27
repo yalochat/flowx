c6feb54f9c1cb934882d09118a92791cce3ec604
José De Paz <josemanuel@kuak.me>
2017-03-26 18:55:04 -0600

    [UPDATE] Add promises to caching

--- index.js
+++ index.js
@@ -13,154 +13,158 @@ const INITIAL_STATE = 0
 
 const client = new Catbox.Client(CatboxMemory)
 
-client.start((err) => {
-  if (err) {
-    throw err
-  }
+module.exports.new = () => {
+  return new Promise((resolve, reject) => {
+    client.start((err) => {
+      if (err) {
+        reject(err)
+      }
 
-  externals.Instance = (function () {
+      externals.Instance = (function () {
 
-    function Instance(model, emitter, middlewares) {
-      /*this.flow = flow
-      this.id = id*/
-      this.model = model
-      this.actualState = this.model.states[INITIAL_STATE]
-      this.middlewares = []
-      this.internalEmitter = emitter
-      this.middlewares = middlewares
-    }
+        function Instance(model, emitter, middlewares) {
+          /*this.flow = flow
+          this.id = id*/
+          this.model = model
+          this.actualState = this.model.states[INITIAL_STATE]
+          this.middlewares = []
+          this.internalEmitter = emitter
+          this.middlewares = middlewares
+        }
 
-    Instance.prototype.searchNextState = function (stateName) {
-      return new Promise((resolve, reject) => {
-        this.model.states.forEach((state) => {
-          if (state.name === stateName) {
-            return resolve(state)
-          }
-        })
-        return reject(new Error('No default state found'))
-      })
-    }
+        Instance.prototype.searchNextState = function (stateName) {
+          return new Promise((resolve, reject) => {
+            this.model.states.forEach((state) => {
+              if (state.name === stateName) {
+                return resolve(state)
+              }
+            })
+            return reject(new Error('No default state found'))
+          })
+        }
 
-    Instance.prototype.validateTransition = function (transitionName) {
-      return new Promise((resolve, reject) => {
-        if (this.actualState.transitions) {
-          this.actualState.transitions.forEach((transition) => {
-            if (Utils.matchRule(transition.name, transitionName) || Utils.matchRegExp(transition.name, transitionName)) {
-              return resolve(transition)
+        Instance.prototype.validateTransition = function (transitionName) {
+          return new Promise((resolve, reject) => {
+            if (this.actualState.transitions) {
+              this.actualState.transitions.forEach((transition) => {
+                if (Utils.matchRule(transition.name, transitionName) || Utils.matchRegExp(transition.name, transitionName)) {
+                  return resolve(transition)
+                }
+              })
+              return reject(new Error('No se pudo encontrar el estado en las transiciones'))
+            } else {
+              return reject(new Error('No se pudo encontrar el estado en las transiciones'))
             }
           })
-          return reject(new Error('No se pudo encontrar el estado en las transiciones'))
-        } else {
-          return reject(new Error('No se pudo encontrar el estado en las transiciones'))
         }
-      })
-    }
 
-    Instance.prototype.getState = function (data) {
-      return new Promise((resolve, reject) => {
-        if (data && data.action) {
-          this.validateTransition(data.action).then((transition) => {
-            this.searchNextState(transition.to).then((nextState) => {
-              if (transition.use) {
-                this.middlewares[transition.use](this.actualState, (data) => {
-                  this.actualState = nextState
-                  return resolve(this.actualState)
-                })
-              } else {
-                this.actualState = nextState
-                if (this.actualState.onEnter) {
-                  if (this.actualState.onEnter.emit) {
-                    this.internalEmitter.emit(this.actualState.onEnter.emit, this.actualState.onEnter.data)
+        Instance.prototype.getState = function (data) {
+          return new Promise((resolve, reject) => {
+            if (data && data.action) {
+              this.validateTransition(data.action).then((transition) => {
+                this.searchNextState(transition.to).then((nextState) => {
+                  if (transition.use) {
+                    this.middlewares[transition.use](this.actualState, (data) => {
+                      this.actualState = nextState
+                      return resolve(this.actualState)
+                    })
+                  } else {
+                    this.actualState = nextState
+                    if (this.actualState.onEnter) {
+                      if (this.actualState.onEnter.emit) {
+                        this.internalEmitter.emit(this.actualState.onEnter.emit, this.actualState.onEnter.data)
+                      }
+                    }
+                    return resolve(this.actualState)
                   }
-                }
-                return resolve(this.actualState)
-              }
-            })
-          }).catch((err) => {
-            this.searchNextState('default').then((nextState) => {
-              this.actualState = nextState
-              if (this.actualState.onEnter) {
-                if (this.actualState.onEnter.emit) {
-                  this.internalEmitter.emit(this.actualState.onEnter.emit, this.actualState.onEnter.data)
-                }
-              }
+                })
+              }).catch((err) => {
+                this.searchNextState('default').then((nextState) => {
+                  this.actualState = nextState
+                  if (this.actualState.onEnter) {
+                    if (this.actualState.onEnter.emit) {
+                      this.internalEmitter.emit(this.actualState.onEnter.emit, this.actualState.onEnter.data)
+                    }
+                  }
+                  return resolve(this.actualState)
+                }).catch((err) => {
+                  return resolve({ template: err })
+                })
+              })
+            } else {
               return resolve(this.actualState)
-            }).catch((err) => {
-              return resolve({ template: err })
+            }
+          })
+        }
+
+        return Instance
+      })()
+
+      externals.Flow = (function () {
+
+        function Flow(name, model) {
+          this.name = name
+          this.model = model
+          this.instances = []
+          this.middlewares = []
+          this.internalEmitter = new flowEmitter();
+        }
+
+        Flow.prototype.newInstance = function (id) {
+          //this.instances[id] = new externals.Instance(this.model, this.internalEmitter, this.middlewares)
+          //return this.instances[id]
+          return new Promise((resolve, reject) => {
+            var newInstance = new externals.Instance(this.model, this.internalEmitter, this.middlewares)
+            client.set(id, newInstance, this.model.ttl, (err) => {
+              if (err) {
+                reject(err)
+              }
+              resolve(newInstance)
             })
           })
-        } else {
-          return resolve(this.actualState)
         }
-      })
-    }
 
-    return Instance
-  })()
+        Flow.prototype.addInstance = function (instance) {
+          //this.instance[instance.id] = instance
+          //return this.instances[instance.id]
+          return new Promise((resolve, reject) => {
+            client.set(instance.id, instance, this.model.ttl, (err) => {
+              if (err) {
+                reject(err)
+              }
+              resolve(instance)
+            })
+          })
+        }
 
-  externals.Flow = (function () {
+        Flow.prototype.getInstance = function (id) {
+          //return this.instances[id]
+          return new Promise((resolve, reject) => {
+            client.get(id, (err, cached) => {
+              if (err) {
+                reject(err)
+              } else if (!cached) {
+                reject(new Error('Data not found'))
+              }
+              resolve(cached.item)
+            })
+          })
+        }
 
-    function Flow(name, model) {
-      this.name = name
-      this.model = model
-      this.instances = []
-      this.middlewares = []
-      this.internalEmitter = new flowEmitter();
-    }
+        Flow.prototype.on = function (eventName, eventFunction) {
+          this.internalEmitter.on(eventName, eventFunction)
+        }
 
-    Flow.prototype.newInstance = function (id) {
-      //this.instances[id] = new externals.Instance(this.model, this.internalEmitter, this.middlewares)
-      //return this.instances[id]
-      return new Promise((resolve, reject) => {
-        var newInstance = new externals.Instance(this.model, this.internalEmitter, this.middlewares)
-        client.set(id, newInstance, this.model.ttl, (err) => {
-          if (err){
-            reject(err)
-          }
-          resolve(newInstance)
-        })
-      })
-    }
+        Flow.prototype.register = function (name, fn) {
+          this.middlewares[name] = fn
+        }
 
-    Flow.prototype.addInstance = function (instance) {
-      //this.instance[instance.id] = instance
-      //return this.instances[instance.id]
-      return new Promise((resolve, reject) => {
-        client.set(instance.id, instance, this.model.ttl, (err) => {
-          if (err){
-            reject(err)
-          }
-          resolve(instance)
-        })
-      })
-    }
+        return Flow
 
-    Flow.prototype.getInstance = function (id) {
-      //return this.instances[id]
-      return new Promise((resolve, reject) => {
-        client.get(id, (err, cached) => {
-          if (err){
-            reject(err)
-          }else if (!cached){
-            reject(new Error('Data not found'))
-          }
-          resolve(cached.item)
-        })
-      })
-    }
+      })()
 
-    Flow.prototype.on = function (eventName, eventFunction) {
-      this.internalEmitter.on(eventName, eventFunction)
-    }
+      resolve(externals)
 
-    Flow.prototype.register = function (name, fn) {
-      this.middlewares[name] = fn
-    }
-
-    return Flow
-
-  })()
-
-  module.exports = externals
-
-})
+    })
+  })
+}

335782d5a0ef4717d41375f8d84fe081b752bc10
Byron Morales <byoms23@gmail.com>
2019-01-21 18:39:16 -0600

    [UPDATE] Add eslint to test file.

--- test/index.test.js
+++ test/index.test.js
@@ -1,4 +1,6 @@
-'use strict'
+// const fx = require('fixtures')
+// const nock = require('nock')
+// const sinon = require('sinon')
 
 const Flowx = require('../')
 const Util = require('../lib/utils')
@@ -96,174 +98,154 @@ const data = {
     }
   ]
 }
-
+console.log(data)
 const preparedData = Util.prepareModel(data).states.toJS()
+const watchdogBaseUri = process.env.WATCHDOG_BASE_URI
 
-test('Get current state without action', () => {
-  return Flowx.new().then((flowxServer) => {
+let instance
+
+// const fakeRequestWatchdog = () => {
+//   nock(watchdogBaseUri)
+//     .get('/domains/test/intents/search?numIntents=3')
+//     .reply(200, {
+//       "domain": "test",
+//       "results": [
+//           {
+//               "classificationId": 3524063,
+//               "entities": [],
+//               "id": 4,
+//               "intents": [
+//                   {
+//                       "category": "buy-ticket",
+//                       "categoryDescription": "Compra de boleto",
+//                       "confidence": 0.6926555037498474
+//                   },
+//                   {
+//                       "category": "promo",
+//                       "categoryDescription": "Promociones",
+//                       "confidence": 0.26473885774612427
+//                   }
+//               ]
+//           }
+//       ]
+//   })
+// }
+
+beforeEach(() => (Flowx.new()
+  .then((flowxServer) => {
+    // console.log('Flowx server created')
     const flow = new flowxServer.Flow('myFlow', data)
     const key = {
       id: '111',
-      segment: 'test'
+      segment: 'test',
     }
-    return flow.getInstance(key).then((bot) => {
-      return flow.getState(bot, {}).then((state) => {
-        expect(state.state).toEqual(preparedData[0])
+    return flow.getInstance(key)
+      .then((bot) => {
+        // console.log('Flowx instance created.')
+        instance = { flow, bot }
       })
-    })
+  })
+))
+
+test('Get current state without action', () => {
+  const { flow, bot } = instance
+  return flow.getState(bot, {}).then((state) => {
+    expect(state.state).toEqual(preparedData[0])
   })
 })
 
 test('Get state with action', () => {
-  return Flowx.new().then((flowxServer) => {
-    const flow = new flowxServer.Flow('myFlow', data)
-    const key = {
-      id: '111',
-      segment: 'test'
-    }
-    return flow.getInstance(key).then((bot) => {
-      return flow.getState(bot, { action: 'toState2' }).then((state) => {
-        expect(state.state).toEqual(preparedData[1])
-      })
-    })
+  const { flow, bot } = instance
+  return flow.getState(bot, { action: 'toState2' }).then((state) => {
+    expect(state.state).toEqual(preparedData[1])
   })
 })
 
 test('Get state with action regExp', () => {
-  return Flowx.new().then((flowxServer) => {
-    const flow = new flowxServer.Flow('myFlow', data)
-    const key = {
-      id: '111',
-      segment: 'test'
-    }
-    return flow.getInstance(key).then((bot) => {
-      return flow.getState(bot, { action: '3' }).then((state) => {
-        expect(state.state).toEqual(preparedData[2])
-      })
-    })
+  const { flow, bot } = instance
+  return flow.getState(bot, { action: '3' }).then((state) => {
+    expect(state.state).toEqual(preparedData[2])
   })
 })
 
 test('Get state with action wildcard', () => {
-  return Flowx.new().then((flowxServer) => {
-    const flow = new flowxServer.Flow('myFlow', data)
-    const key = {
-      id: '111',
-      segment: 'test'
-    }
-    return flow.getInstance(key).then((bot) => {
-      return flow.getState(bot, { action: 'abc' }).then((state) => {
-        expect(state.state).toEqual(preparedData[0])
-      })
-    })
+  const { flow, bot } = instance
+  return flow.getState(bot, { action: 'abc' }).then((state) => {
+    expect(state.state).toEqual(preparedData[0])
   })
 })
 
 test.skip('Get global state', () => {
-  return Flowx.new().then((flowxServer) => {
-    const flow = new flowxServer.Flow('myFlow', data)
-    const key = {
-      id: '111',
-      segment: 'test'
-    }
-    return flow.getInstance(key).then((bot) => {
-      console.log('bot: %j', bot)
-      return flow.getState(bot, { action: 'globalState' }).then((state) => {
-        expect(state.state).toEqual(preparedData[3])
-      })
-    })
+  const { flow, bot } = instance
+  return flow.getState(bot, { action: 'globalState' }).then((state) => {
+    expect(state.state).toEqual(preparedData[3])
   })
 })
 
 test('Get default state', () => {
-  return Flowx.new().then((flowxServer) => {
-    const flow = new flowxServer.Flow('myFlow', data)
-    const key = {
-      id: '111',
-      segment: 'test'
-    }
-    return flow.getInstance(key).then((bot) => {
-      return flow.getState(bot, { action: 'test' }).then((state) => {
-        expect(state.state).toEqual(preparedData[4])
-      })
-    })
+  const { flow, bot } = instance
+  return flow.getState(bot, { action: 'test' }).then((state) => {
+    expect(state.state).toEqual(preparedData[4])
   })
 })
 
 test('Get state using global transition', () => {
-  return Flowx.new().then((flowxServer) => {
-    const flow = new flowxServer.Flow('myFlow', data)
-    const key = {
-      id: '111',
-      segment: 'test'
-    }
-    return flow.getInstance(key).then((bot) => {
-      return flow.getState(bot, { action: 'toState1' }).then((state) => {
-        expect(state.state).toEqual(preparedData[0])
-      })
-    })
+  const { flow, bot } = instance
+  return flow.getState(bot, { action: 'toState1' }).then((state) => {
+    expect(state.state).toEqual(preparedData[0])
   })
 })
 
 test('Get state with confidence', () => {
-  return Flowx.new().then((flowxServer) => {
-    const flow = new flowxServer.Flow('myFlow', data)
-    const key = {
-      id: '111',
-      segment: 'test'
-    }
-    return flow.getInstance(key).then((bot) => {
-      const actions = [
-        {
-          type: 'test',
-          value: 'toState3',
-          confidence: 0.6
-        }
-      ]
-      return flow.getState(bot, { action: actions }).then((state) => {
-        expect(state.state).toEqual(preparedData[2])
-      })
-    })
+  const { flow, bot } = instance
+  const actions = [
+    {
+      type: 'test',
+      value: 'toState3',
+      confidence: 0.6,
+    },
+  ]
+  return flow.getState(bot, { action: actions }).then((state) => {
+    expect(state.state).toEqual(preparedData[2])
   })
 })
 
 test('Get state with actions array without type', () => {
-  return Flowx.new().then((flowxServer) => {
-    const flow = new flowxServer.Flow('myFlow', data)
-    const key = {
-      id: '111',
-      segment: 'test'
-    }
-    return flow.getInstance(key).then((bot) => {
-      const actions = [
-        {
-          value: 'toState2'
-        }
-      ]
-      return flow.getState(bot, { action: actions }).then((state) => {
-        expect(state.state).toEqual(preparedData[1])
-      })
-    })
+  const { flow, bot } = instance
+  const actions = [
+    {
+      value: 'toState2',
+    },
+  ]
+  return flow.getState(bot, { action: actions }).then((state) => {
+    expect(state.state).toEqual(preparedData[1])
   })
 })
 
 test('Get state with actions array with type without confidence', () => {
-  return Flowx.new().then((flowxServer) => {
-    const flow = new flowxServer.Flow('myFlow', data)
-    const key = {
-      id: '111',
-      segment: 'test'
-    }
-    return flow.getInstance(key).then((bot) => {
-      const actions = [
-        {
-          value: 'init',
-          type: 'user'
-        }
-      ]
-      return flow.getState(bot, { action: actions }).then((state) => {
-        expect(state.state).toEqual(preparedData[5])
-      })
-    })
+  const { flow, bot } = instance
+  const actions = [
+    {
+      value: 'init',
+      type: 'user',
+    },
+  ]
+  return flow.getState(bot, { action: actions }).then((state) => {
+    expect(state.state).toEqual(preparedData[5])
   })
 })
+
+// test.skip('Get state with a watchdog transition', () => {
+//   fakeRequestWatchdog()
+//   const { flow, bot } = instance
+//   const actions = [
+//     {
+//       value: 'init',
+//       type: 'user',
+//     },
+//   ]
+//
+//   return flow.getState(bot, { action: actions }).then((state) => {
+//     expect(state.state).toEqual(preparedData[5])
+//   })
+// })
